17a321f8a236d1fe21c17b1ad0aec57fb9461988
saidamst2003 <saida2003mst@gmail.com>
2024-12-17 15:25:39 +0100

    j'ajoute gestion Des Apprenants et switch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-public class Main {
+class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choix = -1;
@@ -15,12 +15,15 @@ public class Main {
             choix = scanner.nextInt();//bach n9raw dakchi lidekhel motaghaayer ondiro lih 7ifed fe choix
             switch (choix) {
                 case 1:
+                    gestionDesApprenants();
                     System.out.println("Vous avez choisi Gestion des Apprenants. ");
+
                     break;
                 case 2:
 
 
                     System.out.println("Vous avez choisi Gestion des Formateurs.");
+
                     break;
                 case 3:
 
@@ -40,4 +43,46 @@ public class Main {
         scanner.close();
     }
 
+
+public static void gestionDesApprenants(){
+    Scanner scanner = new Scanner(System.in);
+    int choix = -1;
+
+    while(choix !=0){
+        System.out.println("-------GESTION DES APPRENANTS------");
+        System.out.println("1. Ajouter un apprenant");
+        System.out.println("2. Afficher tous les apprenants");
+        System.out.println("3. Supprimer un Apprenant");
+        System.out.println("0. Retour au Menu Principal");
+        System.out.println("votre choix: ");
+ choix = scanner.nextInt();// kat9ra lina achno 5tar utilisateur
+        switch (choix) {
+            case 1:
+                ajouterApprenant();// dern ahna methode bach t5lina nzido methode jdida1
+                break;
+            case 2:
+                afficherApprenants();
+                break;
+            case 3:
+                supprimerApprenant();
+                break;
+            case 0:
+                System.out.println("Retour au Menu Principal.");
+                break;
+            default:
+                System.out.println("Choix invalide. Veuillez réessayer.");
+        }
+    }
 }
+
+    private static void supprimerApprenant() {
+    }
+
+    private static void afficherApprenants() {
+    }
+
+    private static void ajouterApprenant() {
+    }
+
+}
+   
