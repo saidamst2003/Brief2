31eb510dd3c0b8748fd3c6b5c4012062d071cf29
saidamst2003 <saida2003mst@gmail.com>
2024-12-19 10:31:28 +0100

    creation des classes apprenant.

--- src/Apprenant.java
+++ src/Apprenant.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Apprenant {
+public class Apprenant extends Personne {
 public  Classe classe;
-public  (arryliste)
+public  ArrayList<Double> notes ;
+
+    public Apprenant(int Id, String nom, String prenom, String email,Classe claasse,ArrayList<notes>) {
+        super(Id, nom, prenom, email);
+         this.classe = claasse;
+         this.notes = new ArrayList<>();
+    }
+    public class getclaasse()// getId: khaatjib lina classe
+    {
+        return classe;
+    }
+    public void setclaasse(Classe classe) {//setID :  tatmodifier
+      this.classe = classe ;
+    }
+    public ArrayList<Double> getNotes(){// getId: khaatjib lina classe
+        return notes;
+    }
+
+
+    @Override // kandiroh afach kanjibo chi methode men classe objet okankhliwha kima hiya
+    public String toString() {//ay7Aja ghadi te retourna ra ha string
+        return "Apprenant{" +
+                "Id=" + Id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
 
 }
